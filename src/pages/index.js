0eec983a4abb358fd2428129da70143f27ae4918
Schmerb <mike.schmerbeck@gmail.com>
2020-10-26 16:08:59 -0400

    updated jsconfig

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 export default function Home() {
   return (
